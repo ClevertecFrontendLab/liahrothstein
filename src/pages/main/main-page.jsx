95151d9bb2685cd5c03f5554591ed9ae4057330e
Dmitry Novikov <liahrothstein@gmail.com>
2023-02-16 01:10:09 +0300

    lines window was done

--- src/pages/main/main-page.jsx
+++ src/pages/main/main-page.jsx
@@ -6,8 +6,8 @@ import classNames from "classnames";
 
 import search from './assets/search.png';
 import filterButton from './assets/filter-button.png';
-import buttonIconOn from './assets/button-icon-on.png';
-import buttonIconOff from './assets/button-icon-off.png';
+import blocksButton from './assets/blocks-button.png';
+import linesButton from './assets/lines-button.png';
 import miniFilter from './assets/mini-filter.png';
 import miniSearch from './assets/mini-search.png';
 import miniCloseSearch from './assets/mini-close-search.png';
@@ -18,8 +18,14 @@ import { useGetBooksQuery } from '../../redux';
 import { FourStars } from '../../components/stars/four-stars';
 import { Loader } from '../../components/loader';
 import { ErrorMessage } from '../../components/error-message';
+import { FiveStars } from '../../components/stars/five-stars';
+import { ThreeStars } from '../../components/stars/three-stars';
+import { TwoStars } from '../../components/stars/two-stars';
+import { OneStar } from '../../components/stars/one-star';
 
 export function MainPage () {
+    const [isActive, setActive] = useState(true);
+
     const [isSearchOpen, toggleSearch] = useState(false);
     const {data = [], isError, isLoading} = useGetBooksQuery();
 
@@ -48,8 +54,16 @@ export function MainPage () {
                     <div className="miniFilter"><img src={miniFilter} alt="filter" /></div>
                 </div>
                 <div className={classNames('buttonIcons', {searchOpen: isSearchOpen})}>
-                    <div className="buttonIcon" data-test-id='button-menu-view-window'><img src={buttonIconOn} alt="" /></div>
-                    <div className="buttonIcon" data-test-id='button-menu-view-list'><img src={buttonIconOff} alt="" /></div>
+                    <button type='button' onClick={() => {setActive(true)}}>
+                        <div className={classNames('buttonIcon', {blocksActive: (isActive === true) ? 'active' : ''})} data-test-id='button-menu-view-window'>
+                            <img src={blocksButton} alt="" />
+                        </div>
+                    </button>
+                    <button type='button' onClick={() => {setActive(false)}}>
+                        <div className={classNames('buttonIcon', {linesActive: (isActive === false) ? 'active' : ''})} data-test-id='button-menu-view-list'>
+                            <img src={linesButton} alt="" />
+                        </div>
+                    </button>
                 </div>
                 <div className={classNames('searchAfterClick', {searchOpen: isSearchOpen})}>
                     <input type="text" placeholder='Поиск книги или автора…' data-test-id='input-search' />
@@ -60,19 +74,52 @@ export function MainPage () {
                     </div>
                 </div>
             </div>
-            <div className={classNames('bookIcons', {loader: isLoading})}>
+            <div className={classNames('bookIcons', {loader: isLoading}, {blocksActive: (isActive === true) ? 'active' : ''})}>
                 {data.map(icon => (
                     <div key={icon.id} className='bookIcon'>
                         <Link to={`/books/all/${icon.id}`} id={icon.id}>
                     <div className="imageOfBook">
                         <img src={`https://strapi.cleverland.by${icon?.image?.url}`} alt="book" />
                     </div>
-                    <FourStars />
+                    {(Math.round(icon.rating) === 5) ? <FiveStars /> :
+                     (Math.round(icon.rating) === 4) ? <FourStars /> :
+                     (Math.round(icon.rating) === 3) ? <ThreeStars /> :
+                     (Math.round(icon.rating) === 2) ? <TwoStars /> :
+                     (Math.round(icon.rating) === 1) ? <OneStar /> :
+                     <div className='noStars'>ещё нет оценок</div>
+                    }
                         <div className="nameOfBook">{icon.title}</div>
                     <div className="author">{icon.authors}, {icon.issueYear}</div>
                         <div className="button">
                             <button type='button' data-test-id='card'>Забронировать</button>
                         </div>
+                    </Link>
+                    </div>
+                ))}
+            </div>
+            <div className={classNames('bookIcons', {loader: isLoading}, {linesActive: (isActive === false) ? 'active' : ''})}>
+                {data.map(icon => (
+                    <div key={icon.id} className='bookIcon'>
+                        <Link to={`/books/all/${icon.id}`} id={icon.id}>
+                    <div className="imageOfBook">
+                        <img src={`https://strapi.cleverland.by${icon?.image?.url}`} alt="book" />
+                    </div>
+                    <div className="all">
+                        <div className="nameOfBook">{icon.title}</div>
+                    <div className="author">{icon.authors}, {icon.issueYear}</div>
+                    <div className="starsWithButton">
+                    {(Math.round(icon.rating) === 5) ? <FiveStars /> :
+                     (Math.round(icon.rating) === 4) ? <FourStars /> :
+                     (Math.round(icon.rating) === 3) ? <ThreeStars /> :
+                     (Math.round(icon.rating) === 2) ? <TwoStars /> :
+                     (Math.round(icon.rating) === 1) ? <OneStar /> :
+                     <div className='noStars'>ещё нет оценок</div>
+                    }
+                        <div className="button">
+                            <button type='button' data-test-id='card'>Забронировать</button>
+                        </div>
+                    </div>
+                    </div>
                     </Link>
                     </div>
                 ))}
