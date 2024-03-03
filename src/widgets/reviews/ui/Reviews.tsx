5ff84d16a7bcfe2a557a2b9675268bd188058b52
liahrothstein <liahrothstein@gmail.com>
2024-03-03 17:05:28 +0300

    feat: add write first comment styles

--- src/widgets/reviews/ui/Reviews.tsx
+++ src/widgets/reviews/ui/Reviews.tsx
@@ -14,15 +14,22 @@ import './Reviews.scss';
 export function Reviews() {
     const { state } = useLocation();
     const [isAllView, setIsAllView] = useState<boolean>(false);
+    const [isNoComments, setIsNoComments] = useState<boolean>(false);
     const [comments, setComments] = useState<CommentType[]>(state);
 
     useEffect(() => {
         sortComments(state, setComments, isAllView);
     }, [isAllView]);
 
+    useEffect(() => {
+        if (comments.length === 0) {
+            setIsNoComments(true);
+        }
+    }, [comments]);
+
     return (
-        <section className="reviews">
-            {comments.map((comment: CommentType) => (
+        <section className={(isNoComments) ? "reviews noComments" : "reviews"}>
+            {(!isNoComments) && comments.map((comment: CommentType) => (
                 <Comment
                     key={comment.id}
                     avatar={comment.imageSrc}
@@ -31,16 +38,21 @@ export function Reviews() {
                     createdAt={comment.createdAt}
                     message={comment.message} />
             ))}
+            {(isNoComments) &&
+                <article className="writeFirstComment">
+                    <h2 className="title">Оставьте свой отзыв первым</h2>
+                    <p className="subtitle">Вы можете быть первым, кто оставит отзыв об этом фитнесс приложении.<br />Поделитесь своим мнением и опытом с другими пользователями,<br />и помогите им сделать правильный выбор.</p>
+                </article>}
             <div className="buttons">
                 <WriteComment
                     sortComments={sortComments}
                     setComments={setComments}
                     isAllView={isAllView}
                     state={state} />
-                <Button
+                {(!isNoComments) && <Button
                     className="allView"
                     title={(isAllView) ? 'Свернуть все отзывы' : "Развернуть все отзывы"}
-                    onClickHandler={() => (switcher(isAllView, setIsAllView))} />
+                    onClickHandler={() => (switcher(isAllView, setIsAllView))} />}
             </div>
         </section>
     )
